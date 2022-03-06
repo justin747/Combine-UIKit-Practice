b894fcc76022a90947031295d6025ab59a81bd46
Justin747 <justin747@gmail.com>
2022-03-05 16:35:30 -0800

    Updated ViewController
    Added basic test fields to test combine. Project will be revisited later

--- Combine + UIKit Practice/ViewController.swift
+++ Combine + UIKit Practice/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import Combine
+
+extension Notification.Name {
+    static let newBlogPost = Notification.Name("newPost")
+}
+
+struct BlogPost {
+    let title: String
+}
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var blogTextField: UITextField!
+    @IBOutlet var publishButton: UIButton!
+    @IBOutlet var subscribedLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        publishButton.addTarget(self, action: #selector(publishButtonTapped), for: .primaryActionTriggered)
+        
+        let publisher = NotificationCenter.Publisher(center: .default, name: .newBlogPost, object: nil)
+            .map { (notification) -> String? in
+                return (notification.object as? BlogPost)?.title ?? ""
+            }
+        
+        let subscriber = Subscribers.Assign(object: subscribedLabel, keyPath: \.text)
+        publisher.subscribe(subscriber)
     }
-
-
+    
+    @objc func publishButtonTapped(_ sender: UIButton) {
+        // Post the notification
+        let title = blogTextField.text ?? "Coming soon"
+        let blogPost = BlogPost(title: title)
+        NotificationCenter.default.post(name: .newBlogPost, object: blogPost)
+    }
 }
 
